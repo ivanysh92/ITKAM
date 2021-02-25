e6fa47ee9ec188d7aef28971a8d448ad68caa1b8
ivanysh92 <pit_bull2007@mail.ru>
2021-02-25 20:42:08 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://regnum.ru/uploads/pictures/news/2015/11/13/regnum_picture_144736430464322_normal.png" alt="" />
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div className="content" >
+      <div>
+        <img src="https://images.wallpapersden.com/image/download/landscape-lake-mountains_am5nbmWUmZqaraWkpJRobWllrWdma2U.jpg" alt="" />
+      </div>
+      <div>
+        ava+description
+    </div>
+      <div>
+        My post
+        <div>
+          New post
+        </div>
+      </div>
+      <div>
+        Post 1
+    </div>
+      <div>
+        Post 2
+    </div>
+    </div>
+  )
+}
+
+export default Profile;
